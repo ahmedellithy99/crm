994efe0dd889fa3e00f2ff5688412752bb80b740
ahmedellithy99 <ahmedelithy99@gmail.com>
2023-12-25 14:49:32 +0200

    adding models and migrations

---
+++ app/Models/Project.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    public function client():BelongsTo
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function users():BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
